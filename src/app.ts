99f7300e0fa0c60f940ab60e1777014d3e1831d0
Rubens Albukrk <rubensf4rda@gmail.com>
2024-03-10 13:53:23 -0300

    fixes processes.json

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express, { Request, Response } from 'express';
 import Routes from './routes/routes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
